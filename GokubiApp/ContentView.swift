6872875326b23d2f68a5d1d129351c568324057b
alzea <alzea.arafat@icloud.com>
2024-11-10 14:33:55 +0700

    Initial Commit

---
+++ GokubiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GokubiApp
+//
+//  Created by Alzea Arafat on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GokubiApp/GokubiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GokubiAppApp.swift
+//  GokubiApp
+//
+//  Created by Alzea Arafat on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GokubiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
