1078ffeced4cfb114c4db49911441348639c3aa5
alzea <alzea.arafat@icloud.com>
2024-11-29 11:42:42 +0700

    Add Game Stats

--- GokubiApp/Views/Screens/GameDetailScreenView.swift
+++ GokubiApp/Views/Screens/GameDetailScreenView.swift
@@ -61,36 +61,155 @@ struct GameDetailScreenView: View {
                     }
                 }
                 
-                VStack(alignment: .leading, spacing: 4) {
-                    HStack {
-                        Text(game.title)
+                VStack(alignment: .leading, spacing: 34) {
+                    VStack(alignment: .leading, spacing: 2) {
+                        HStack {
+                            Text(game.title)
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .fontDesign(.rounded)
+                            
+                            Spacer()
+                            
+                            if game.completed {
+                                BadgeView(
+                                    icon: "medal.fill",
+                                    label: "Completed",
+                                    labelColor: .white,
+                                    backgroundColor: .green
+                                )
+                            } else {
+                                BadgeView(
+                                    label: "On Progress",
+                                    labelColor: .secondary,
+                                    backgroundColor: .slate200
+                                )
+                            }
+                        }
+                        Text("By \(game.developer)")
+                            .foregroundStyle(.secondary)
+                            .fontDesign(.rounded)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 16) {
+                        Text("Gaming Stats")
                             .font(.title3)
                             .fontWeight(.semibold)
-                            .fontDesign(.rounded)
                         
-                        Spacer()
-                        
-                        if game.completed {
-                            BadgeView(
-                                icon: "medal.fill",
-                                label: "Completed",
-                                labelColor: .white,
-                                backgroundColor: .green
+                        HStack {
+                            // PLAYTIME
+                            VStack(alignment: .leading, spacing: 8) {
+                                HStack {
+                                    Image(systemName: "clock.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 16, height: 16)
+                                    
+                                    Text("Playtime")
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .fontDesign(.rounded)
+                                }
+                                
+                                Text("\(game.playtime.formatted(.number.precision(.fractionLength(0)))) hours")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .fontDesign(.rounded)
+                            }
+                            .padding(.trailing, 40)
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                                    .stroke(.slate400, lineWidth: 0.5)
                             )
-                        } else {
-                            BadgeView(
-                                label: "On Progress",
-                                labelColor: .secondary,
-                                backgroundColor: .slate200
+                            
+                            // RATING
+                            VStack(alignment: .leading, spacing: 8) {
+                                HStack {
+                                    Image(systemName: "star.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 16, height: 16)
+                                    
+                                    Text("Rating")
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .fontDesign(.rounded)
+                                }
+                                
+                                Text("\(game.rating) out of 5")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .fontDesign(.rounded)
+                            }
+                            .padding(.trailing, 40)
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                                    .stroke(.slate400, lineWidth: 0.5)
+                            )
+                        }
+                        
+                        HStack {
+                            // PLAYTIME
+                            VStack(alignment: .leading, spacing: 8) {
+                                HStack {
+                                    Image(systemName: "clock.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 16, height: 16)
+                                    
+                                    Text("Genre")
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .fontDesign(.rounded)
+                                }
+                                
+                                Text("\(game.genre)")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .fontDesign(.rounded)
+                            }
+                            .padding(.trailing, 40)
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                                    .stroke(.slate400, lineWidth: 0.5)
+                            )
+                            
+                            // RATING
+                            VStack(alignment: .leading, spacing: 8) {
+                                HStack {
+                                    Image(systemName: "star.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 16, height: 16)
+                                    
+                                    Text("Platform")
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .fontDesign(.rounded)
+                                }
+                                
+                                Text("\(game.platforms)")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .fontDesign(.rounded)
+                            }
+                            .padding(.trailing, 40)
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                                    .stroke(.slate400, lineWidth: 0.5)
                             )
                         }
                     }
-                    
-                    Text("By \(game.developer)")
-                        .foregroundStyle(.secondary)
-                        .fontDesign(.rounded)
                 }
-                .padding(24)
+                .padding(16)
             }
         }
     }
